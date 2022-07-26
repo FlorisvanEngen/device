771b9cccb5d4d9f6b681f6d58ee908cf33cbab46
FlorisVanEngen <floris.van.engen@connectionsystems.nl>
2022-07-26 11:40:58 +0200

    autocomplete bugfix

--- resources/views/pages/register/create.blade.php
+++ resources/views/pages/register/create.blade.php
@@ -1,10 +1,10 @@
 <x-layout>
     <h1>Register</h1>
-    <form method="POST" action="/register">
+    <form method="POST" action="/register" autocomplete="off">
         @csrf
-        <x-form.input name="name" required/>
-        <x-form.input name="email" type="email" required/>
-        <x-form.input name="password" type="password" required/>
+        <x-form.input name="name" required />
+        <x-form.input name="email" type="email" required />
+        <x-form.input name="password" type="password" required />
         <x-form.button>
             Register
         </x-form.button>
